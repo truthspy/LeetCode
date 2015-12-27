9d5391d1158bcf4df4353d30ee9c5350c2a61108
Rongrong Qi <qirongrong0831@163.com>
2015-12-27 23:08:44 +0100

    Create 258_Add_Digits.cpp

---
+++ 258_Add_Digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num < 10)
+            return num;
+        else {
+            int tmp = 0;
+            while(num != 0) {
+                tmp += num % 10;
+                num = num / 10;
+            }
+            return addDigits(tmp);
+        }
+            
+    }
+};
