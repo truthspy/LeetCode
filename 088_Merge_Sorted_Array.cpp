a6d53423a694ef3652f3a9a99c16846d1a5c588e
Rongrong Qi <qirongrong0831@163.com>
2016-02-23 11:17:25 +0800

    Update 088_Merge_Sorted_Array.cpp
    看discuss后优化

--- 088_Merge_Sorted_Array.cpp
+++ 088_Merge_Sorted_Array.cpp
@@ -1,32 +1,30 @@
+//看discuss后解答：
+//不是链表，可以从后往前倒着合并
+//这样一定不会覆盖掉nums1中的原有数据
+//最后拷贝剩余部分可以只考虑nums2情况
+//复杂度o(m+n)
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> nums12;
-        //注意vector用法：
-        //未放入数据之前不能使用[]取值，放入数据用push_back
-        int pos1 = 0;
-        int pos2 = 0;
-
-        while(pos1 < m && pos2 < n) {
-            if(nums1[pos1] < nums2[pos2]) {
-                nums12.push_back(nums1[pos1]);
-                pos1 ++;
+        int pos1 = m - 1;
+        int pos2 = n - 1;
+        int pos = m + n - 1;
+        while(pos1 >= 0 && pos2 >= 0) {
+            if(nums1[pos1] > nums2[pos2]) {
+                nums1[pos] = nums1[pos1];
+                pos1 --;
             }
             else {
-                nums12.push_back(nums2[pos2]);
-                pos2 ++;
+                nums1[pos] = nums2[pos2];
+                pos2 --;
             }
+            pos --;
         }
-        while(pos1 < m) {
-            nums12.push_back(nums1[pos1]);
-            pos1 ++;
-        }
-        while(pos2 < n) {
-            nums12.push_back(nums2[pos2]);
-            pos2 ++;
-        }
-        for(int i = 0; i < (m + n); i ++) {
-            nums1[i] = nums12[i];
+        while(pos2 >= 0) {
+            nums1[pos] = nums2[pos2];
+            pos2 --;
+            pos --;
         }
     }
 };
