d9bafb1833dfc552be58bb361dc743687a00f148
Rongrong Qi <qirongrong0831@163.com>
2016-02-26 00:14:37 +0800

    Update 145_Binary_Tree_Postorder_Traversal.cpp

--- 145_Binary_Tree_Postorder_Traversal.cpp
+++ 145_Binary_Tree_Postorder_Traversal.cpp
@@ -7,6 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+//这个会改变树结构，不好！！！
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
@@ -29,6 +31,36 @@ public:
                 node->right = NULL;
                 node = tmp;
             }
+        }
+        return postorder;
+    }
+};
+
+
+//new version
+//这个不会改变树结构
+//通过记录前一个pop的结点实现
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+        stack<TreeNode*> treeStack;
+        TreeNode* node = root;
+        TreeNode* pre =  NULL;
+        while(node != NULL || !treeStack.empty()) {
+            while(node != NULL) {
+                treeStack.push(node);
+                node = node->left;
+            }
+            node = treeStack.top();
+            if(node->right == NULL || pre == node->right) {
+                postorder.push_back(node->val);
+                treeStack.pop();
+                pre = node;
+                node = NULL;
+            }
+            else
+                node = node->right;
         }
         return postorder;
     }
