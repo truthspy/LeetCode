5ff7c8caba4b5c96ebb6587a1264f9f0da225da9
Rongrong Qi <qirongrong0831@163.com>
2016-07-17 15:36:44 +0800

    Update 347_Top_K_Frequent_Elements.cpp

--- Session2/347_Top_K_Frequent_Elements.cpp
+++ Session2/347_Top_K_Frequent_Elements.cpp
@@ -1,3 +1,4 @@
+// Method1:
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -18,6 +19,33 @@ public:
             k --;
             res.push_back(q.top().second);
             q.pop();
+        }
+        return res;
+    }
+};
+// Method2:
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int size = nums.size();
+        for(int i = 0; i < size; i ++) {
+            mp[nums[i]] ++;
+        }
+        vector<vector<int>> buckets(size);
+        for(auto num : mp) {
+            //因为至少有一个，不减1会越界
+            buckets[num.second - 1].push_back(num.first);
+        }
+        int curNum = 0;
+        vector<int> res;
+        for(int i = size - 1; i >= 0; i --) {
+            for(int j = 0; j < buckets[i].size(); j ++) {
+                curNum ++;
+                res.push_back(buckets[i][j]);
+                if(curNum == k)
+                    return res;
+            }
         }
         return res;
     }
