7d4f0a17b368f0c37b18c7cc2b55bcf1b5655d3f
Rongrong Qi <qirongrong0831@163.com>
2016-07-31 15:57:17 +0800

    Create 326_Power_of_Three.cpp

---
+++ Session2/326_Power_of_Three.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0)
+            return false;
+        if(n == 1)
+            return true;
+        while(n > 2) {
+            if(n % 3 != 0)
+                return false;
+            n = n / 3;
+        }
+        return n == 1;
+    }
+};
