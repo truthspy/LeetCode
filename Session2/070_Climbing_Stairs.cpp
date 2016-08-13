259a88c9e58342c3368b223f5f60fdf803755dab
Rongrong Qi <qirongrong0831@163.com>
2016-08-13 19:41:29 +0800

    Create 070_Climbing_Stairs.cpp

---
+++ Session2/070_Climbing_Stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> steps(n, 0);
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        steps[0] = 1;
+        steps[1] = 2;
+        for(int i = 2; i < n; i ++)
+            steps[i] = steps[i - 1] + steps[i - 2];
+        return steps[n - 1];
+    }
+};
