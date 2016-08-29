8b8efc7e143b0431171d71b6744f54c8f0857bfc
Rongrong Qi <qirongrong0831@163.com>
2016-08-29 21:39:09 +0800

    Update 088_Merge_Sorted_Array.cpp

--- Session2/088_Merge_Sorted_Array.cpp
+++ Session2/088_Merge_Sorted_Array.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //从后往前会少去复制的环节
+        if(n == 0)
+            return;
+        int i = m - 1, j = n - 1, pos = m + n - 1;
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] >= nums2[j]) {
+                nums1[pos] = nums1[i];
+                i --;
+            }
+            else {
+                nums1[pos] = nums2[j];
+                j --;
+            }
+            pos --;
+        }
+        while(j >= 0) {
+            nums1[pos] = nums2[j];
+            j --;
+            pos --;
+        }
+    }
+};
+/*
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         if(n == 0)
             return;
         for(int i = m - 1; i >= 0; i --) {
@@ -25,4 +51,4 @@ public:
             pos ++;
         }
     }
-};
+};*/
