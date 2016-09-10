8faf5a8813d6ae02e842065a5b3c6bcbdc052143
Rongrong Qi <qirongrong0831@163.com>
2016-09-10 22:24:37 +0800

    Create 382_Linked_List_Random_Node.cpp

---
+++ Session2/382_Linked_List_Random_Node.cpp
@@ -0,0 +1,40 @@
+// 蓄水池问题
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* head;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int res = head->val;
+        ListNode* cur = head->next;
+        int i = 2;
+        while(cur) {
+            int pos = rand() % i;
+            if(pos == 0)
+                res = cur->val;
+            cur = cur->next;
+            i ++;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
