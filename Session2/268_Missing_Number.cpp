be62ef60ca976246dbf3a60ac890cc6c19299d45
Rongrong Qi <qirongrong0831@163.com>
2016-10-09 23:17:05 +0800

    Create 268_Missing_Number.cpp

---
+++ Session2/268_Missing_Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        for(int i = 0; i < n; i ++) {
+            res = res ^ nums[i];
+            res = res ^ i;
+        }
+        res = res ^ n;
+        return res;
+    }
+};
