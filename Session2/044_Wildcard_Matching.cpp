27eafec0d811cbf55876d32b88ceb6d302b1eb4f
Rongrong Qi <qirongrong0831@163.com>
2016-10-11 17:23:59 +0800

    Create 044_Wildcard_Matching.cpp

---
+++ Session2/044_Wildcard_Matching.cpp
@@ -0,0 +1,26 @@
+// TODO 贪心算法 快
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int len1 = s.length();
+        int len2 = p.length();
+        vector<vector<bool>> dp(len1 + 1, vector<bool>(len2 + 1, false));
+        dp[0][0] = true;
+        for(int i = 1; i <= len2; i ++) {
+            if(p[i - 1] != '*')
+                break;
+            dp[0][i] = true;
+        }
+        for(int i = 1; i <= len1; i ++) {
+            for(int j = 1; j <= len2; j ++) {
+                if(p[j - 1] == '?')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if(p[j - 1] == '*')
+                    dp[i][j] = (dp[i - 1][j - 1] || dp[i][j - 1] || dp[i - 1][j]);
+                else
+                    dp[i][j] = (dp[i - 1][j - 1] && p[j - 1] == s[i - 1]);
+            }
+        }
+        return dp[len1][len2];
+    }
+};
